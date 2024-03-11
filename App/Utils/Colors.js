e145e4477e2e934f5c8246fd8da836465960ee1e
GITvivek1 <vivekkr9308@gmail.com>
2024-03-11 20:51:38 +0530

    whole at once

---
+++ App/Utils/Colors.js
@@ -0,0 +1,8 @@
+export default{
+    PRIMARY:'#96E2DF',
+    SECONDRY:'#2196AC',
+    BUTTONS:'#2196AC',
+    GRAY:'#898989',
+    WHITE:'#ffffff',
+    BLACK:'#000000',
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#EV-charging-station-app
+ EV-Charging -Station-app
+# EV-charging-station-app
